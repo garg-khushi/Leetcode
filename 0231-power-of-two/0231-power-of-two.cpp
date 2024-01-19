6b2eeedf0bdea9b3e5dc641550ccfe8c7a95b43a
garg-khushi <139839680+garg-khushi@users.noreply.github.com>
2024-01-19 10:42:46 +0530

    Time: 2 ms (39.24%), Space: 6.2 MB (81.78%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+         if(n==1 ){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        else{
+            if(n%2!=0)
+                return false;
+            else{
+                return isPowerOfTwo(n/2);
+            }
+        }
+        
+    }
+};
