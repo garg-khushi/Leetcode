a8d0221912e389886de2a984abb702668bb1fc3e
garg-khushi <139839680+garg-khushi@users.noreply.github.com>
2024-01-19 10:41:35 +0530

    Time: 5 ms (12.94%), Space: 6.5 MB (10.91%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==1 ){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        else{
+            if(n%4!=0)
+                return false;
+            else{
+                return isPowerOfFour(n/4);
+            }
+        }
+        
+    }
+};
