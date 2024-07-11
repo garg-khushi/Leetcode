c7a0bf36bcadeee401386ff9cbd9d3993396fd1c
garg-khushi <139839680+garg-khushi@users.noreply.github.com>
2024-07-11 08:02:34 +0530

    Time: 3 ms (49.64%), Space: 7.5 MB (84.88%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+    void reverseSubstring(string &s, int a, int b) {
+        while (a < b) {
+            char temp = s[a];
+            s[a] = s[b];
+            s[b] = temp;
+            a++;
+            b--;
+        }
+    }
+    
     string reverseParentheses(string s) {
         stack<int> st;
         for (int i = 0; i < s.size(); i++) {
@@ -8,17 +18,17 @@ public:
             } else if (s[i] == ')') {
                 int start = st.top();
                 st.pop();
-                reverse(s.begin() + start + 1, s.begin() + i);
+                reverseSubstring(s, start + 1, i - 1);
             }
         }
-
+        
         string result;
         for (char c : s) {
             if (c != '(' && c != ')') {
                 result += c;
             }
         }
-
+        
         return result;
     }
 };
