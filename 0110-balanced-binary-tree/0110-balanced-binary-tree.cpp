96fc54984a61c85a95ec94471c6d3263bddf7d28
garg-khushi <139839680+garg-khushi@users.noreply.github.com>
2024-07-17 12:36:47 +0530

    Time: 10 ms (48.44%), Space: 21.9 MB (5.48%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,17 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-int dfsHeight (TreeNode *root) {
-        if (root == NULL) return 0;
+    int dfsheight(TreeNode *root){
+        if(root==NULL ) return 0;
+        int lh=dfsheight(root->left);
+        if(lh==-1) return -1;
+        int rh=dfsheight(root->right);
+        if(rh==-1) return -1;
         
-        int leftHeight = dfsHeight (root -> left);
-        if (leftHeight == -1) return -1;
-        int rightHeight = dfsHeight (root -> right);
-        if (rightHeight == -1) return -1;
-        
-        if (abs(leftHeight - rightHeight) > 1)  return -1;
-        return max (leftHeight, rightHeight) + 1;
+        if(abs(lh-rh)>1) return -1;
+        return max(lh,rh)+1;
     }
-    bool isBalanced(TreeNode *root) {
-        return dfsHeight (root) != -1;
+    bool isBalanced(TreeNode* root) {
+        if(dfsheight(root)==-1)
+            return 0;
+        else return 1;
     }
 };
+// class solution {
+// public:
+// int dfsHeight (TreeNode *root) {
+//         if (root == NULL) return 0;
+        
+//         int leftHeight = dfsHeight (root -> left);
+//         if (leftHeight == -1) return -1;
+//         int rightHeight = dfsHeight (root -> right);
+//         if (rightHeight == -1) return -1;
+        
+//         if (abs(leftHeight - rightHeight) > 1)  return -1;
+//         return max (leftHeight, rightHeight) + 1;
+//     }
+//     bool isBalanced(TreeNode *root) {
+//         return dfsHeight (root) != -1;
+//     }
+// };
